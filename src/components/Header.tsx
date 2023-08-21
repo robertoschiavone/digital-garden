61c33b2b402798e5e514c4c419b39c97078976cb
Roberto Schiavone <hello@robertoschiavone.io>
2023-08-21 16:00:00 +0200

    fix: favicon :lipstick:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header = () =>
     <header className="container mx-auto">
         <nav className="pt-10 px-5 lg:px-20">
             <Link href="/">
-                <Image src="/images/icon.png" alt="icon" width="64"
-                       height="64" />
+                <Image src="/icon.svg" loading="eager" alt="icon" width="64"
+                       height="64"/>
             </Link>
         </nav>
     </header>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,23 @@
 import React from "react"
 
-import { Head, Html, Main, NextScript } from "next/document"
+import {Head, Html, Main, NextScript} from "next/document"
 
 const Document = () =>
     <Html lang="en">
         <Head>
-            <meta charSet="utf-8" />
-            <meta name="robots" content="follow, index" />
-            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <link rel="icon" type="image/x-icon" href="/images/icon.png" />
+            <meta charSet="utf-8"/>
+            <link rel="manifest" href="/manifest.webmanifest"/>
+            <link rel="icon" href="/favicon.ico" sizes="32x32"/>
+            <link rel="icon" href="/icon.svg" type="image/svg+xml"/>
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+
+            <meta name="robots" content="follow, index"/>
+            <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
         </Head>
         <body>
 
-        <Main />
-        <NextScript />
+        <Main/>
+        <NextScript/>
         </body>
     </Html>
 
